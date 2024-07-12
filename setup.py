8b2eadb398f16e03e315cd72f0fff25673f9047c
CristopherAlejandoRuizBouchez <100699422+CristopherAlejandoRuizBouchez@users.noreply.github.com>
2024-07-12 00:56:47 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'optimizacion_Cristopher',
+      version = '0.1' ,
+      description = ' Proyecto de optimizacion con varios codigos para dicha optimizacion',
+      url = 'https://github.com/CristopherAlejandoRuizBouchez/Proyecto-Optimizacion',
+      author = 'Cristopher Alejandro Ruiz Bouchez',
+      author_email = 'cristopherruizbouchez@gmail.com',
+      packages=['funcion','metodos_multivariable','metodos_una_variable'],
+      zip_safe = False)
+
