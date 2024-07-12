6b0f65a889eb3a41bd8583a54380ebb54f806eae
CristopherAlejandoRuizBouchez <100699422+CristopherAlejandoRuizBouchez@users.noreply.github.com>
2024-07-12 00:56:21 -0600

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Proyecto Final Optimizacion'
+copyright = '2024, Cristopher Alejandro Ruiz Bouchez'
+author = 'Cristopher Alejandro Ruiz Bouchez'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser","sphinx.ext.autodoc",'sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
